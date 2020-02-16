dd0a0622010c4a63ab8efa82b5be7b6f58787d32
Alexandre duval <alexandre.duval@student-cs.fr>
2020-02-16 19:38:43 +0100

    new data extractor

--- src/dataset_generation/metadata_extractor.py
+++ src/dataset_generation/metadata_extractor.py
@@ -1,7 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 16 18:04:35 2020
 
-###########################################
+@author: alexandreduval
+"""
+
 # Set up 
-###########################################
 #pip install gutenberg
 #git clone https://github.com/c-w/Gutenberg.github
 #cd Gutenberg
@@ -9,28 +14,35 @@
 #brew install berkeley-db4
 #pip install .
 
+
 # Import libraries 
 from gutenberg.acquire import load_etext, get_metadata_cache
 from gutenberg.cleanup import strip_headers 
 from gutenberg.query import get_etexts, list_supported_metadatas, get_metadata
 
-# Check what kind of metadata we can get
-print(list_supported_metadatas()) # prints (u'author', u'formaturi', u'language', ...)
 
 # Download a text 
 text = strip_headers(load_etext(2701)).strip()
 print(text)  # prints 'MOBY DICK; OR THE WHALE\n\nBy Herman Melville ...'
 
-# Populate the metadata cache
+# Populate the metadata cache - long operation 
 cache = get_metadata_cache()
 cache.populate()
 
-# Extract metadata from that file 
-print(get_metadata('title', 2701))  # prints frozenset([u'Moby Dick; Or, The Whale'])
-print(get_metadata('author', 2701)) # prints frozenset([u'Melville, Hermann'])
+# Check what kind of metadata we can get
+print(list_supported_metadatas()) # prints (u'author', u'formaturi', u'language', ...)
 
+# Extract metadata from that file
+book_id = 1000 
+print(get_metadata('title', book_id))  
+print(get_metadata('author', book_id)) 
+print(get_metadata('subject', book_id)) 
+print(get_metadata('language', book_id))
+
+# Find book id wrt to some filters
 print(get_etexts('title', 'Moby Dick; Or, The Whale'))  # prints frozenset([2701, ...])
-print(get_etexts('author', 'Melville, Hermann'))        # prints frozenset([2701, ...])
+print(get_etexts('subject', 'Fiction'))        # prints frozenset([2701, ...])
+print(get_etexts('author', 'Melville, Herman'))
 
 """
 # Cache with more control 
