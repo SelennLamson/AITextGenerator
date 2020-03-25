3933cedb930c7f4ba7d1b03f29eea480fe1b5521
Alexandre duval <alexandre.duval@student-cs.fr>
2020-03-25 18:00:56 +0100

    bert summariser

--- tests_alex.py
+++ tests_alex.py
@@ -7,18 +7,21 @@ from importlib import reload
 from src.json_generation import paragraph_preprocessing
 from src.json_generation import ent_sum_preprocessing
 from src.flexible_models import FlexibleBERTNER
+from src.flexible_models import FlexibleBERTSum
 from src.utils import *
 # reload(ent_sum_preprocessing)
 
 # Split into paragraphs
-paragraph_preprocessing.separate_paragraphs_all_files(overwrite=False)
+# paragraph_preprocessing.separate_paragraphs_all_files(overwrite=False)
 
 # Create ent_sum template
-ent_sum_preprocessing.prepare_json_templates(True)
+# ent_sum_preprocessing.prepare_json_templates(True)
 
 # Perform NER
-model = FlexibleBERTNER(BERT_NER_LARGE, batch_size=128, max_length=2000)
-ent_sum_preprocessing.perform_ner_on_file(model)
+# model = FlexibleBERTNER(BERT_NER_LARGE, batch_size=128, max_length=2000)
+# ent_sum_preprocessing.perform_ner_on_file(model)
 
-
+# Summarise
+model_sum = FlexibleBERTSum()
+ent_sum_preprocessing.add_summaries([model_sum])
 
