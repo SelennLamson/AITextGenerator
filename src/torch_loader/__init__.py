6554d41f86ede042af4505c11c52e12997b24ca2
Gaël de Léséleuc <dldk.gael@gmail.com>
2020-03-25 21:33:57 +0100

    add torch dataset

---
+++ src/torch_loader/__init__.py
@@ -0,0 +1,3 @@
+from .dataset_from_json import DatasetFromJson
+from .dataset_from_repo import DatasetFromRepo
+from .vectorize_paragraph import VectorizeParagraph
--- src/torch_loader/dataset_from_json.py
+++ src/torch_loader/dataset_from_json.py
@@ -1,35 +1,7 @@
-from torch.utils.data import Dataset, ConcatDataset
-import os
+from torch.utils.data import Dataset
 import json
 
-class DatasetFromJsonRepo(Dataset):
-    """
-    DatasetFromRepo :
-    generate vectorize samples (P1, P3, context ; P2) from a repo of JSON files
-
-    main idea :
-        1/ we generate a dataset for each JSON file
-        2/ we concacenate all this dataset in a big one using ConcatDataset
-            (all this operation will be proceed on the fly)
-    """
-    def __init__(self, path, transform):
-        """
-        :param path: repository of all JSON files
-        :param transform: function to transform paragraph into a vectorized form
-        """
-        # We only use json file so before scrapping we check if the extention is json
-        datasets = [DatasetFromJsonFile(path + json_files, transform) for json_files in os.listdir(path)
-                                                                      if json_files[-4:] == "json"]
-        self.dataset = ConcatDataset(datasets)
-
-    def __getitem__(self, idx):
-        return self.dataset[idx]
-
-    def __len__(self):
-        return len(self.dataset)
-
-
-class DatasetFromJsonFile(Dataset):
+class DatasetFromJson(Dataset):
     """
     DatasetFromFile :
     generate vectorize samples (P1, P3, context ; P2) from a single JSON file
