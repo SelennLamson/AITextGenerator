39f957c9600ec4047c11279cdc1741afde7f186d
Gaël de Léséleuc <dldk.gael@gmail.com>
2020-03-29 15:09:53 +0200

    update will custom special tokens

---
+++ src/model_training/__init__.py
@@ -0,0 +1,1 @@
+from .update_model import add_special_tokens
--- src/model_training/update_model.py
+++ src/model_training/update_model.py
@@ -2,22 +2,23 @@
 Function to define special tokens (ie : CTRL code) and update the model and tokenizer
 """
 
-def add_special_tokens(model, tokenizer):
+def add_special_tokens(model=None, tokenizer=None):
     """
     update in place the model and tokenizer to take into account special tokens
     :param model: GPT2 model from huggingface
     :param tokenizer: GPT2 tokenizer from huggingface
     """
 
-    tokenizer.add_special_tokens({'pad_token': '[PAD]',
-                                  'eos_token': '[EOS]',
-                                  'additional_special_tokens': ['[P1]',
-                                                                '[P3]',
-                                                                '[S]',
-                                                                '[M]',
-                                                                '[L]',
-                                                                '[T]',
-                                                                '[Sum]',
-                                                                '[Ent]']})
-
-    model.resize_token_embeddings(len(tokenizer))
+    if tokenizer:
+        tokenizer.add_special_tokens({'pad_token': '[PAD]',
+                                      'eos_token': '[EOS]',
+                                      'additional_special_tokens': ['[P1]',
+                                                                    '[P3]',
+                                                                    '[S]',
+                                                                    '[M]',
+                                                                    '[L]',
+                                                                    '[T]',
+                                                                    '[Sum]',
+                                                                    '[Ent]']})
+    if model:
+        model.resize_token_embeddings(len(tokenizer))
