27b47aff9ce0c7bd5b1d295a802a683949868fc6
Gaël de Léséleuc <dldk.gael@gmail.com>
2020-03-31 22:38:22 +0200

    modify text_generation to use FlexibleGPT2

--- src/model_use/text_generation.py
+++ src/model_use/text_generation.py
@@ -3,16 +3,15 @@ from src.torch_loader import VectorizeParagraph
 class TextGeneration:
     """
     TextGeneration will be used to generate text from input given by webserver
+    It combines a FlexibleGPT2 model and a specific vectorizer that will transform
+    the data from webserver into the correct inputs id
     """
-    def __init__(self, GPT2_model, decoding_strategy):
+    def __init__(self, GPT2_model):
         """
         TextGeneration is initialized with a fine-tuned GPT2 model and a decoding_strategy
         :param GPT2_model: FlexibleGPT2
-        :param decoding_strategy: dict of parameters for huggingface transformers.generate methods
         """
         self.GPT2_model = GPT2_model
-        self.decoding_strategy = decoding_strategy
-        self.max_length = decoding_strategy['max_length']
         self.vectorizer = VectorizeParagraph(tokenizer=self.GPT2_model.tokenizer, block_size=1020, train_mode=False)
 
     def __call__(self, context_input, nb_samples=1):
@@ -21,6 +20,5 @@ class TextGeneration:
         :param nb_samples: number of different generated text that will be output
         :return: list[str] : list of generated text
         """
-
         input_ids = self.vectorizer(context_input.vectorizer_input_format(), mode="generation")
         return self.GPT2_model.predict(input_ids, nb_samples)
