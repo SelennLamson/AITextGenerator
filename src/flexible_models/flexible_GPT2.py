b05a8dc0d9ebc44a41990854c1cfbd9fac5a8654
Gaël de Léséleuc <dldk.gael@gmail.com>
2020-03-31 22:05:32 +0200

    add flexible GPT2 model

--- src/flexible_models/flexible_GPT2.py
+++ src/flexible_models/flexible_GPT2.py
@@ -1,7 +1,7 @@
 from .flexible_model import FlexibleModel
 
 class FlexibleGPT2(FlexibleModel):
-    def __init__(self, gpt2_model, gpt2_tokenizer, decoding_strategy):
+    def __init__(self, model, tokenizer, decoding_strategy):
         """
         Initializes a GPT2 model.
         :param gpt2_model: huggingface gpt2 transformers
@@ -9,8 +9,8 @@ class FlexibleGPT2(FlexibleModel):
         :param decoding_strategy: dict of parameters for huggingface transformers.generate methods
         """
         super().__init__()
-        self.GPT2_model = gpt2_model
-        self.GPT2_tokenizer = gpt2_tokenizer
+        self.model = model
+        self.tokenizer = tokenizer
         self.decoding_strategy = decoding_strategy
         self.max_length = decoding_strategy['max_length']
 
@@ -21,10 +21,10 @@ class FlexibleGPT2(FlexibleModel):
         :param nb_samples: nb_sample to generate for each input example
         :return: list of strings of len batch_size * nb_samples
         """
-        mask = (input_ids != self.GPT2_tokenizer.pad_token_id).long()
+        mask = (input_ids != self.tokenizer.pad_token_id).long()
         self.decoding_strategy['max_length'] = self.max_length + input_ids.shape[1]
-        outputs_id = self.GPT2_model.generate(input_ids=input_ids,
-                                              pad_token_id=self.GPT2_tokenizer.eos_token_id,
+        outputs_id = self.model.generate(input_ids=input_ids,
+                                              pad_token_id=self.tokenizer.eos_token_id,
                                               attention_mask=mask,
                                               num_return_sequences=nb_samples,
                                               **self.decoding_strategy)
@@ -33,5 +33,5 @@ class FlexibleGPT2(FlexibleModel):
         # this is because transformers.generate methods also return the input part
         truncated_outputs_id = outputs_id[:, input_ids.shape[1]:]
 
-        return [self.GPT2_tokenizer.decode(truncated_outputs_id[i], skip_special_tokens=True)
+        return [self.tokenizer.decode(truncated_outputs_id[i], skip_special_tokens=True)
                 for i in range(outputs_id.shape[0])]
--- src/model_use/text_generation.py
+++ src/model_use/text_generation.py
@@ -4,18 +4,16 @@ class TextGeneration:
     """
     TextGeneration will be used to generate text from input given by webserver
     """
-    def __init__(self, GPT2_model, GPT2_tokenizer, decoding_strategy):
+    def __init__(self, GPT2_model, decoding_strategy):
         """
         TextGeneration is initialized with a fine-tuned GPT2 model and a decoding_strategy
-        :param GPT2_model: huggingface GPT2 transformers model
-        :param GPT2_tokenizer: huggingface GPT2 tokeziner
+        :param GPT2_model: FlexibleGPT2
         :param decoding_strategy: dict of parameters for huggingface transformers.generate methods
         """
         self.GPT2_model = GPT2_model
-        self.GPT2_tokenizer = GPT2_tokenizer
         self.decoding_strategy = decoding_strategy
         self.max_length = decoding_strategy['max_length']
-        self.vectorizer = VectorizeParagraph(tokenizer=self.GPT2_tokenizer, block_size=1020, train_mode=False)
+        self.vectorizer = VectorizeParagraph(tokenizer=self.GPT2_model.tokenizer, block_size=1020, train_mode=False)
 
     def __call__(self, context_input, nb_samples=1):
         """
@@ -24,18 +22,5 @@ class TextGeneration:
         :return: list[str] : list of generated text
         """
 
-        input_ids, _ = self.vectorizer(context_input.vectorizer_input_format())
-        mask = (input_ids != self.GPT2_tokenizer.pad_token_id).long()
-        self.decoding_strategy['max_length'] = self.max_length + input_ids.shape[1]
-        outputs_id = self.GPT2_model.generate(input_ids=input_ids,
-                                              pad_token_id=self.GPT2_tokenizer.eos_token_id,
-                                              attention_mask=mask,
-                                              num_return_sequences=nb_samples,
-                                              **self.decoding_strategy)
-
-        # only keep the token corresponding to the generation part
-        # this is because transformers.generate methods also return the input part
-        truncated_outputs_id = outputs_id[:, input_ids.shape[1]:]
-
-        return [self.GPT2_tokenizer.decode(truncated_outputs_id[i], skip_special_tokens=True)
-                for i in range(outputs_id.shape[0])]
+        input_ids = self.vectorizer(context_input.vectorizer_input_format(), mode="generation")
+        return self.GPT2_model.predict(input_ids, nb_samples)
