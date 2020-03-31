35f1d6b83ba606ff211f3cce1261ce8f84e201a3
Gaël de Léséleuc <dldk.gael@gmail.com>
2020-03-31 12:20:17 +0200

    test for generation

--- src/model_use/text_generation.py
+++ src/model_use/text_generation.py
@@ -1,15 +1,19 @@
 from transformers import GPT2LMHeadModel, GPT2Tokenizer
+from src.model_training.update_model import add_special_tokens
 
 if __name__ == "__main__":
-    tokenizer = GPT2Tokenizer.from_pretrained('gpt2-large')
-    model = GPT2LMHeadModel.from_pretrained('gpt2-large')
+    tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
+    tokenizer.padding_side = 'left'
+    model = GPT2LMHeadModel.from_pretrained('gpt2')
+    add_special_tokens(model=model, tokenizer=tokenizer)
     input_ids = tokenizer.encode('Mirror, mirror on the wall, who\'s the fairest of them all? Alex, Gael or Thomas ?',
                                  return_tensors='pt')
+    print(input_ids)
 
     sample_output = model.generate(
         input_ids,
+        max_lenght=50,
         do_sample=True,
-        max_length=50,
         top_p=0.92,
         top_k=0
     )
