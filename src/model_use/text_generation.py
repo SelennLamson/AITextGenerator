4871b1bd216f6708b2abbb093ccdf687b975bd4b
Gaël de Léséleuc <dldk.gael@gmail.com>
2020-03-31 16:52:18 +0200

    add text generation class

--- src/model_use/text_generation.py
+++ src/model_use/text_generation.py
@@ -1,22 +1,41 @@
-from transformers import GPT2LMHeadModel, GPT2Tokenizer
-from src.model_training.update_model import add_special_tokens
+from src.torch_loader import VectorizeParagraph
 
-if __name__ == "__main__":
-    tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
-    tokenizer.padding_side = 'left'
-    model = GPT2LMHeadModel.from_pretrained('gpt2')
-    add_special_tokens(model=model, tokenizer=tokenizer)
-    input_ids = tokenizer.encode('Mirror, mirror on the wall, who\'s the fairest of them all? Alex, Gael or Thomas ?',
-                                 return_tensors='pt')
-    print(input_ids)
+class TextGeneration:
+    """
+    TextGeneration will be used to generate text from input given by webserver
+    """
+    def __init__(self, GPT2_model, GPT2_tokenizer, decoding_strategy):
+        """
+        TextGeneration is initialized with a fine-tuned GPT2 model and a decoding_strategy
+        :param GPT2_model: huggingface GPT2 transformers model
+        :param GPT2_tokenizer: huggingface GPT2 tokeziner
+        :param decoding_strategy: dict of parameters for huggingface transformers.generate methods
+        """
+        self.GPT2_model = GPT2_model
+        self.GPT2_tokenizer = GPT2_tokenizer
+        self.decoding_strategy = decoding_strategy
+        self.max_length = decoding_strategy['max_length']
+        self.vectorizer = VectorizeParagraph(tokenizer=self.GPT2_tokenizer, block_size=1020, train_mode=False)
 
-    sample_output = model.generate(
-        input_ids,
-        max_lenght=50,
-        do_sample=True,
-        top_p=0.92,
-        top_k=0
-    )
+    def __call__(self, context_input, nb_samples=1):
+        """
+        :param context_input: [GenerationInput] instance containing the context for the generation
+        :param nb_samples: number of different generated text that will be output
+        :return: list[str] : list of generated text
+        """
 
-    print("Output:\n" + 100 * '-')
-    print(tokenizer.decode(sample_output[0], skip_special_tokens=True))
+        input_ids, _ = self.vectorizer(context_input.vectorizer_input_format())
+        mask = (input_ids != self.GPT2_tokenizer.pad_token_id).long()
+        self.decoding_strategy['max_length'] = self.max_length + input_ids.shape[1]
+        outputs_id = self.GPT2_model.generate(input_ids=input_ids,
+                                              pad_token_id=self.GPT2_tokenizer.eos_token_id,
+                                              attention_mask=mask,
+                                              num_return_sequences=nb_samples,
+                                              **self.decoding_strategy)
+
+        # only keep the token corresponding to the generation part
+        # this is because transformers.generate methods also return the input part
+        truncated_outputs_id = outputs_id[:, input_ids.shape[1]:]
+
+        return [self.GPT2_tokenizer.decode(truncated_outputs_id[i], skip_special_tokens=True)
+                for i in range(outputs_id.shape[0])]
