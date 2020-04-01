4b96c9167778ff5e543791349da2e9fba38109cf
Gaël de Léséleuc <dldk.gael@gmail.com>
2020-04-01 11:55:08 +0200

    add flexibleGPT2 to import

--- src/flexible_models/__init__.py
+++ src/flexible_models/__init__.py
@@ -6,3 +6,4 @@ from .flexible_bert_ner import FlexibleBERTNER
 from .flexible_bert_embed import FlexibleBERTEmbed
 
 from .paragraph_parser import ParagraphParser
+from .flexible_GPT2 import FlexibleGPT2
