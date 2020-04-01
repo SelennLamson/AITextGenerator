2f679844f52521247e8a3d25e74ce2d8b65aa72b
Gaël de Léséleuc <dldk.gael@gmail.com>
2020-04-01 12:03:28 +0200

    re-test code

--- old_test_files/test_evaluation.py
+++ old_test_files/test_evaluation.py
@@ -1,5 +1,5 @@
 from src.model_evaluation import Evaluation
-from src.flexible_models.flexible_bert_ner import FlexibleBERTNER
+from src.flexible_models import FlexibleBERTNER, FlexibleGPT2
 
 from transformers import GPT2LMHeadModel, GPT2Tokenizer
 
@@ -18,18 +18,16 @@ DECODING_STRATEGY = {
 }
 
 if __name__ == '__main__':
-    GPT2_tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
-    GPT2_tokenizer.pad_token = GPT2_tokenizer.eos_token
+    GPT2_model = FlexibleGPT2(model=GPT2LMHeadModel.from_pretrained('gpt2'),
+                              tokenizer=GPT2Tokenizer.from_pretrained('gpt2'),
+                              decoding_strategy=DECODING_STRATEGY)
 
-    GPT2_model = GPT2LMHeadModel.from_pretrained('gpt2')
     BERT_NER_model = FlexibleBERTNER(BERT_NER_BASE, batch_size=5, max_length=128)
 
-    evaluation = Evaluation(GPT_model=GPT2_model,
-                            GPT_tokenizer=GPT2_tokenizer,
+    evaluation = Evaluation(GPT2_model=GPT2_model,
                             BERT_NER_model=BERT_NER_model,
                             path_to_repo=PATH_TO_REPO,
-                            batch_size=1,
-                            decoding_strategy=DECODING_STRATEGY)
+                            batch_size=1)
 
     metrics = evaluation.compute_metrics(nb_examples_to_evaluate=5)
     print(metrics)
---
+++ test_vectorization.py
@@ -0,0 +1,32 @@
+from src.torch_loader import VectorizeParagraph, DatasetFromRepo
+from src.model_training import add_special_tokens
+from transformers import GPT2Tokenizer
+import random
+
+"""
+Script to test the vectorization module in train and evaluation mode 
+The idea is to randomly take one example from the dataset and then :
+1/ vectorize the concataned sentence
+2/ print the de-vectorize version and qualitatively check if it is ok
+"""
+
+JSON_FILE_PATH = "data/preproc/"
+
+if __name__ == '__main__':
+    tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
+    add_special_tokens(tokenizer=tokenizer)
+
+    # TEST VECTORIZER IN TRAIN MODE
+    vectorize_paragraph = VectorizeParagraph(tokenizer, block_size=1020, mode='train')
+    novels_dataset = DatasetFromRepo(path=JSON_FILE_PATH, transform=vectorize_paragraph)
+    idx = random.randint(0, len(novels_dataset)-1)
+    print(novels_dataset[idx])
+    print(tokenizer.decode(novels_dataset[idx]))
+
+    print("\n", '-'*100,"\n")
+    # TEST VECTORIZER IN EVAL MODE
+    vectorize_paragraph = VectorizeParagraph(tokenizer, block_size=1020, mode='eval')
+    novels_dataset = DatasetFromRepo(path=JSON_FILE_PATH, transform=vectorize_paragraph)
+    tokenize_context, P2 = novels_dataset[idx]
+    print(tokenizer.decode(tokenize_context), " , ", P2)
+
