cd4d72acf356b2fb50034b6fe22a36a1e0acf025
Gaël de Léséleuc <dldk.gael@gmail.com>
2020-04-06 21:25:28 +0200

    new vectorizer

--- src/torch_loader/vectorize_paragraph.py
+++ src/torch_loader/vectorize_paragraph.py
@@ -1,172 +1,91 @@
 import torch
-from src.utils import get_size_from_chars, SMALL, MEDIUM, LARGE
+from enum import Enum
+from src.utils import get_size_from_chars, SMALL, MEDIUM, LARGE, GPT2_BLOCK_SIZE
+
+class VectorizeMode(Enum):
+    TRAIN = 0
+    EVAL = 1
+    GENERATE = 2
 
 class VectorizeParagraph:
     """
-    class VectorizeParagrah :
-    from {input: (metadata, P1, P3), target: P2} generate {input: X, output: Y}
-    when X and Y are vectorized token tensors obtains using GPT2Tokenizer
-
+    class VectorizeParagrah
     An instance of this class will be callable on {input: (metadata, P1, P3), target: P2}
-    Later it will be possible to configure it (for instance for randomly erase some data)
     """
-    def __init__(self, tokenizer, block_size=1020, mode='train'):
+    def __init__(self, tokenizer, block_size=GPT2_BLOCK_SIZE, mode=VectorizeMode.TRAIN, use_context=True):
         """
-        Initialize GPT2Tokenizer and add specific token
+        Store the parameters of the vectorizer, by default : mode = Train, use_context = True
         :param block_size : int, max sequence_token_len for GPT_2 input
-        :param mode: one value in ['train', 'eval', 'eval_wo_context', 'generation']
+        :param mode: VectorizerMode
+        :param use_context: True to use full context with special token
+                            False to only use P1 without any special token
         """
         self.tokenizer = tokenizer
         self.block_size = block_size
         self.mode = mode
+        self.use_context = use_context
 
     @staticmethod
-    def bin_size(size):
+    def special_token_for_size(size):
         """
         :param size: int <-> size of a paragraph (in number of chars) or SMALL, MEDIUM, LARGE
-        :return: special token [S], [M], [L] <-> corresponding binned size
-        for now only return [M]
+        :return: special token [S], [M], [L] corresponding to the size
         """
         if type(size) == int:
             size = get_size_from_chars(size)
-        if size == SMALL:
-            return ' [S] '
-        if size == MEDIUM:
-            return ' [M] '
-        if size == LARGE:
-            return ' [L] '
+        return ' ' + size.token + ' '
 
     @staticmethod
-    def nb_tokens_to_save_for_P2(size):
+    def concat_context(context_d):
         """
-        :param size: SMALL, MEDIUM OR LARGE
-        :return: int number token that will be save for P2 in input block
-        """
-        return size.mean_tokens + 50
-
-    @staticmethod
-    def concat(input_d):
-        return input_d['P3'] + input_d['Sum'] + input_d['T'] + input_d['Ent'] + \
-               input_d['Size'] + input_d['P1'] + input_d['P2']
-
-    def train_mode(self, input_dict, P2):
-        """
-        Concatanate all the information from sample as a global string in the following order
-           [P3] P3 [Sum] Sum_P2 [T] Theme [Ent] list_of_person [Size] [P1] P1 [P2] P2 [EOS]
-        If needed will truncate P3 then P1 so that the tokenize version of the string is smaller that self.block_size
-
-        :param input_dict [dict] representing the context (see VectorizeParagraph.__call__ for further details)
-        :param  P2 [str]
-        :return: [torch.tensor] tokenize version of the string
-                    [P3] P3 [Sum] Sum_P2 [T] Theme [Ent] list_of_person [Size] [P1] P1 [P2] P2 [EOS]
-        """
-        input_dict['P2'] += self.tokenizer.encode(' ' + P2 + '[EOS]')
-
-        vector_size = sum(map(len, input_dict.values()))
-
-        # If the vector size is not too long, we return it directly
-        if vector_size <= self.block_size:
-            return torch.tensor(self.concat(input_dict))
-
-        size_wo_P3 = vector_size - len(input_dict['P3'])
-        # Else we truncate the P3
-        if size_wo_P3 <= self.block_size:
-            input_dict['P3'] = input_dict['P3'][:(self.block_size - size_wo_P3)]
-            return torch.tensor(self.concat(input_dict))
-
-        size_wo_P3_and_P1 = vector_size - len(input_dict['P3']) - len(input_dict['P1'])
-        # If still not sufficient, we remove P1 and truncate P1 from left but still add the [P1] special token
-        if size_wo_P3_and_P1 <= self.block_size:
-            input_dict['P3'] = []
-            input_dict['P1'] = self.tokenizer.encode('[P1]') + \
-                               input_dict['P1'][self.block_size - size_wo_P3_and_P1 + 1:]
-            return torch.tensor(self.concat(input_dict))
-
-        # It still to big we simply return truncated P2
-        # This case will normally never happen if the dataset have been correctly constructed
-        else:
-            input_id = input_dict['P2'][:min(self.block_size, len(input_dict['P2']))]
-            if self.train_mode:
-                return torch.tensor(input_id)
-
-    def eval_mode(self, input_dict, P2, P3):
-        """
-        Concatanate all the information from sample as a global string in the following order
-            [P3] P3 [Sum] Sum_P2 [T] Theme [ENT] list_of_person [Size] [P1] P1 [P2]
-        If needed will truncate P3 then P1 so that the tokenize version of the string is smaller that self.block_size
-        :param [dict] input_dict representing the context (see VectorizeParagraph.__call__ for further details)
-        :param [str] P2
-        :param [str] P3
-        :return: tupple :
-                    [torch.tensor] tokenize version of the following string ->
-                        [P3] P3 [Sum] Sum_P2 [T] Theme [ENT] list_of_person [Size] [P1] P1 [P2]
-                    [str] P2
-                    [str] P3
-        """
-        vector_size = sum(map(len, input_dict.values())) + len(self.tokenizer.encode(P2))
-        if vector_size <= self.block_size:
-            return torch.tensor(self.concat(input_dict)), P2, P3
-
-        # Else we truncate as in train_mode (first P3, then P1)
-        size_wo_P3 = vector_size - len(input_dict['P3'])
-        if size_wo_P3 <= self.block_size:
-            input_dict['P3'] = input_dict['P3'][:(self.block_size - size_wo_P3)]
-            return torch.tensor(self.concat(input_dict)), P2, P3
-
-        # If still not sufficient, we remove P1 and truncate P1 from left but still add the [P1] special token
-        size_wo_P3_and_P1 = vector_size - len(input_dict['P3']) - len(input_dict['P1'])
-        if size_wo_P3_and_P1 <= self.block_size:
-            input_dict['P3'] = []
-            input_dict['P1'] = self.tokenizer.encode('[P1]') + \
-                               input_dict['P1'][self.block_size - size_wo_P3_and_P1 + 1:]
-            return torch.tensor(self.concat(input_dict)), P2, P3
-
-        # By default we return nothing
-        return torch.tensor(0), "", P3
-
-    def eval_mode_wo_context(self, P1, P2, P3):
-        """
-        :param P1 [str]
-        :param P2 [str]
-        :param P3 [str]
-        :return: tokenize version of P1, P2 as a string, P3 as string
-        if tokenize of P1 + P2 is to big, we truncate P1 at left side
-        """
-        P1_encoded = self.tokenizer.encode(P1)
-        P2_encoded = self.tokenizer.encode(P2)
-
-        if len(P1_encoded) + len(P2_encoded) <= self.block_size:
-            return torch.tensor(P1_encoded), P2, P3
-        else:
-            remaining_space = self.block_size - len(P2_encoded)
-            return torch.tensor(P1_encoded[len(P1_encoded) - remaining_space:]), P2, P3
-
-    def generation_mode(self, input_dict, nb_tokens_saved_for_P2=(MEDIUM.mean_tokens+50)):
-        """
-        Concatanate all the information from sample as a global string in the following order
+        Concatanate the context_dict in the order
             [P3] P3 [Sum] Sum_P2 [T] Theme [Ent] list_of_person [Size] [P1] P1 [P2]
-        :param input_dict
-        :param nb_tokens_saved_for_P2 :  to know how much we let space for P2 generation
-        :return:[torch.tensor] tokenize version of the following string
-                    [P3] P3 [Sum] Sum_P2 [T] Theme [Ent] list_of_person [Size] [P1] P1 [P2]
+        :param context_d: [dict] containing the context
+        :return: concatanate of context_d values in the right order
         """
-        vector_size = sum(map(len, input_dict.values())) + nb_tokens_saved_for_P2
-        if vector_size <= self.block_size:
-            return torch.tensor(self.concat(input_dict))
+        return context_d['P3'] + context_d['Sum'] + context_d['T'] + context_d['Ent'] + \
+               context_d['Size'] + context_d['P1'] + context_d['P2']
 
-        # Else, we let 2/3 of the space for P1 and 1/3 for P3
-        initial_vector_size = len(input_dict['Sum'] + input_dict['metadata']) + nb_tokens_saved_for_P2
-        nb_tokens_left_for_P1 = int((self.block_size - initial_vector_size) * 2/3 - 1)
-        nb_tokens_left_for_P3 = int((self.block_size - initial_vector_size) * 1/3 - 1)
-        input_dict['P1'] = self.tokenizer.encode('[P1] ') + \
-                           input_dict['P1'][nb_tokens_left_for_P1:]
-        input_dict['P3'] = input_dict['P3'][:nb_tokens_left_for_P3]
-        return torch.tensor(self.concat(input_dict))
+    def vectorize(self, context, P2, nb_tokens_for_P2):
+        """
+        :param context: [dict] representing the context
+        :param P2: [str] True P2 or '' in generation_mode
+        :param nb_tokens_for_P2: nb of tokens in true P2 or nb of tokens to saved for P2 in generation mode
+        :return: vectorize version of the dict
+        """
+        nb_tokens_for_context = sum(map(len, context.values()))  # Context = everything except P2
+
+        if self.mode == VectorizeMode.GENERATE:
+            # If the context + input space we must left for P2 is too big
+            # We let 2/3 of the remaining space for P1 and 1/3 for P3
+            if nb_tokens_for_context + nb_tokens_for_P2 >= self.block_size:
+                initial_vector_size = len(context['Sum'] + context['metadata']) + nb_tokens_for_P2
+                nb_tokens_left_for_P1 = int((self.block_size - initial_vector_size) * 2 / 3 - 1)
+                nb_tokens_left_for_P3 = int((self.block_size - initial_vector_size) * 1 / 3 - 1)
+                context['P1'] = self.tokenizer.encode('[P1] ') + context['P1'][nb_tokens_left_for_P1:]  # TRUNCATE P1
+                context['P3'] = context['P3'][:nb_tokens_left_for_P3]  # TRUNCATE P3
+
+        if self.mode == VectorizeMode.TRAIN:
+            # In train mode, P2 is added to the context and the sentence to predict is simplify the full context
+            context['P2'] += self.tokenizer.encode(' ' + P2 + ' [EOS]')
+
+        if self.mode == VectorizeMode.EVAL or self.mode == VectorizeMode.TRAIN:
+            # If the context +  P2 is too big, we first truncate P3 then P1
+            size_wo_P3 = nb_tokens_for_context + nb_tokens_for_P2 - len(context['P3'])
+            if size_wo_P3 > self.block_size: # TODO CHANGE THAT IT IS FALSE !!!! !
+                context['P3'] = context['P3'][:(self.block_size - size_wo_P3)]  # TRUNCATE P3
+
+            size_wo_P3_and_P1 = nb_tokens_for_context + nb_tokens_for_P2 - len(context['P3']) - len(context['P1'])
+            # If still not sufficient, we remove P1 and truncate P1 from left but still add the [P1] special token
+            if size_wo_P3_and_P1 > self.block_size:
+                context['P3'] = []
+                context['P1'] = self.tokenizer.encode('[P1]') + context['P1'][self.block_size - size_wo_P3_and_P1 + 1:]
+
+        return torch.tensor(self.concat_context(context))
 
     def __call__(self, sample):
         """
-        Create an input_dict which format and encode (using GPT2 tokenizer) the data
-        and pass it to train_model, eval_mode or generation_mode depending of self.mode
+        Create an input_dict which formats and encodes (using GPT2 tokenizer) the data
 
         :param sample: tupple (metadata, P1, P3, P2)
             metadata : dict 'title' -> [str], 'author' -> [str], 'genre' -> list[str]
@@ -175,27 +94,53 @@ class VectorizeParagraph:
                 'text' : [str]
                 'summaries' : list[str]
                 'persons': list[str]
+
+        :return:
+            for train mode:
+                with context : [P3] P3 [Sum] Sum_P2 [T] Theme [Ent] list_of_person [Size] [P1] P1 [P2] P2 [EOS]
+                without context : P1 P2
+
+            for eval mode:
+                with context : [P3] P3 [Sum] Sum_P2 [T] Theme [Ent] list_of_person [Size] [P1] P1 [P2], P2_str, P3_str
+                witouht context : P1, P2_str, P3_str
+
+            for generate mode:
+                with context: [P3] P3 [Sum] Sum_P2 [T] Theme [Ent] list_of_person [Size] [P1] P1 [P2]
+                witout context : P1
         """
         metadata, P1, P3, P2 = sample
-        input_dict = dict()
-        input_dict['P1'] = ' [P1] ' + P1['text'] if P1['text'] != "" else ""
-        input_dict['P3'] = ' [P3] ' + P3['text'] if P3['text'] != "" else ""
-        input_dict['P2'] = ' [P2] '
-        input_dict['Sum'] = ' [Sum] ' + ". ".join(P2["summaries"]) if P2["summaries"] != [] else ""
-        input_dict['T'] = ' [T] ' + " - ".join(metadata['genre']) if metadata['genre'] != [] else ""
-        input_dict['Ent'] = ' [Ent] ' + " - ".join(P2['persons']) if P2['persons'] != [] else ""
-        input_dict['Size'] = self.bin_size(P2['size'])
 
-        for key, value in input_dict.items():
-            input_dict[key] = self.tokenizer.encode(value)
+        if self.use_context:
+            context = dict()
+            context['P1'] = ' [P1] ' + P1['text'] if P1['text'] != "" else ""
+            context['P3'] = ' [P3] ' + P3['text'] if P3['text'] != "" else ""
+            context['P2'] = ' [P2] '
+            context['Sum'] = ' [Sum] ' + ". ".join(P2["summaries"]) if P2["summaries"] != [] else ""  # TODO SELECT ONE RANDOMLY
+            context['T'] = ' [T] ' + " - ".join(metadata['genre']) if metadata['genre'] != [] else ""
+            context['Ent'] = ' [Ent] ' + " - ".join(P2['persons']) if P2['persons'] != [] else ""
+            context['Size'] = self.special_token_for_size(P2['size'])
+        else:
+            # if the full context must not used, only P1 will be taken into account without any special tokens
+            context = {'P1': P1['text'], 'P2': '', 'P3': '', 'Sum': '', 'T': '', 'Ent': '', 'Size': ''}
 
-        if self.mode == "train":
-            return self.train_mode(input_dict, P2['text'])
-        elif self.mode == "eval":
-            return self.eval_mode(input_dict, P2['text'], P3['text'])
-        elif self.mode == "eval_wo_context":
-            return self.eval_mode_wo_context(P1['text'], P2['text'], P3['text'])
-        elif self.mode == "generation":
-            return self.generation_mode(input_dict, P2['size'].mean_tokens + 50)
-        elif self.mode == "generation_wo_context":
-            return self.eval_mode_wo_context(P1['text'], "", "")
+        # Encode the context
+        for key, value in context.items():
+            context[key] = self.tokenizer.encode(value)
+
+        # In generate mode, obviously, the true P2 doest not exist
+        P2_text = P2['text'] if self.mode != VectorizeMode.GENERATE else ""
+        P3_text = P3['text']
+
+        # Compute the number of tokens we saved for P2 in the input block
+        if self.mode == VectorizeMode.GENERATE:
+            nb_tokens_for_P2 = P2['size'].mean_tokens + 50
+        else:
+            nb_tokens_for_P2 = len(self.tokenizer.encode(P2_text))
+
+        input_ids = self.vectorize(context, P2_text, nb_tokens_for_P2)
+
+        if self.mode == VectorizeMode.TRAIN or self.mode == VectorizeMode.GENERATE:
+            return input_ids
+        if self.mode == VectorizeMode.EVAL:
+            return input_ids, P2_text, P3_text
+
--- test_vectorization.py
+++ test_vectorization.py
@@ -1,7 +1,10 @@
-from src.torch_loader import VectorizeParagraph, DatasetFromRepo
+from src.torch_loader import VectorizeParagraph, DatasetFromRepo, VectorizeMode
 from src.model_training import add_special_tokens
-from transformers import GPT2Tokenizer
+from src.model_use import GenerationInput
+from src.utils import MEDIUM
+
 import random
+from transformers import GPT2Tokenizer
 
 """
 Script to test the vectorization module in train and evaluation mode 
@@ -15,18 +18,49 @@ JSON_FILE_PATH = "data/preproc/"
 if __name__ == '__main__':
     tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
     add_special_tokens(tokenizer=tokenizer)
+    idx = random.randint(0, len(DatasetFromRepo(path=JSON_FILE_PATH))-1)
 
-    # TEST VECTORIZER IN TRAIN MODE
-    vectorize_paragraph = VectorizeParagraph(tokenizer, block_size=1020, mode='train')
+    print("--- TEST VECTORIZER IN TRAIN MODE WITH FULL CONTEXT ---")
+    vectorize_paragraph = VectorizeParagraph(tokenizer, mode=VectorizeMode.TRAIN, use_context=True)
     novels_dataset = DatasetFromRepo(path=JSON_FILE_PATH, transform=vectorize_paragraph)
-    idx = random.randint(0, len(novels_dataset)-1)
-    print(novels_dataset[idx])
     print(tokenizer.decode(novels_dataset[idx]))
 
-    print("\n", '-'*100,"\n")
-    # TEST VECTORIZER IN EVAL MODE
-    vectorize_paragraph = VectorizeParagraph(tokenizer, block_size=1020, mode='eval')
+    print("--- TEST VECTORIZER IN TRAIN MODE WITHOUT FULL CONTEXT ---")
+    vectorize_paragraph = VectorizeParagraph(tokenizer, mode=VectorizeMode.TRAIN, use_context=False)
     novels_dataset = DatasetFromRepo(path=JSON_FILE_PATH, transform=vectorize_paragraph)
-    tokenize_context, P2, _ = novels_dataset[idx]
-    print(tokenizer.decode(tokenize_context), " , ", P2)
+    idx = random.randint(0, len(novels_dataset)-1)
+    print(tokenizer.decode(novels_dataset[idx]))
 
+    print("\n", '-' * 100, "\n")
+    print("--- TEST VECTORIZER IN EVAL MODE WITH FULL CONTEXT ---")
+    vectorize_paragraph = VectorizeParagraph(tokenizer, mode=VectorizeMode.EVAL, use_context=True)
+    novels_dataset = DatasetFromRepo(path=JSON_FILE_PATH, transform=vectorize_paragraph)
+    idx = random.randint(0, len(novels_dataset)-1)
+    tokenize_context, P2, P3 = novels_dataset[idx]
+    print(tokenizer.decode(tokenize_context))
+
+    print("\n", '-'*100,"\n")
+    print("--- TEST VECTORIZER IN EVAL MODE WITHOUT FULL CONTEXT ---")
+    vectorize_paragraph = VectorizeParagraph(tokenizer, mode=VectorizeMode.EVAL, use_context=False)
+    novels_dataset = DatasetFromRepo(path=JSON_FILE_PATH, transform=vectorize_paragraph)
+    tokenize_context, P2, P3 = novels_dataset[idx]
+    print(tokenizer.decode(tokenize_context))
+
+    context_input = GenerationInput(P1="Ceci est le début de phrase, ",
+                                    P3="et ceci sera la fin de la phrase.",
+                                    genre=["horror"],
+                                    entities=["Gael", "Alex", "Thomas"],
+                                    size=MEDIUM,
+                                    context="Je voudrai parler de ceci")
+
+    print("\n", '-'*100,"\n")
+    print("--- TEST VECTORIZER IN GENERATE MODE WITH FULL CONTEXT ---")
+    vectorize_paragraph = VectorizeParagraph(tokenizer, mode=VectorizeMode.GENERATE, use_context=True)
+    tokenize_context = vectorize_paragraph(context_input.vectorizer_input_format())
+    print(tokenizer.decode(tokenize_context))
+
+    print("\n", '-'*100,"\n")
+    print("--- TEST VECTORIZER IN GENERATE MODE WITHOUT FULL CONTEXT ---")
+    vectorize_paragraph = VectorizeParagraph(tokenizer, mode=VectorizeMode.GENERATE, use_context=False)
+    tokenize_context = vectorize_paragraph(context_input.vectorizer_input_format())
+    print(tokenizer.decode(tokenize_context))
