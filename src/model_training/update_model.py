50ce9515ac879b4befea6dc883ed480fd1ee91f6
Weazel <perso@thomas-lamson.com>
2020-04-07 15:59:07 +0200

    Deleted gpt2-benchmark + updated update_model with utils's sizes

--- src/model_training/update_model.py
+++ src/model_training/update_model.py
@@ -2,6 +2,8 @@
 Function to define special tokens (ie : CTRL code) and update the model and tokenizer
 """
 
+from src.utils import *
+
 def add_special_tokens(model=None, tokenizer=None):
     """
     update in place the model and tokenizer to take into account special tokens
@@ -9,15 +11,14 @@ def add_special_tokens(model=None, tokenizer=None):
     :param tokenizer: GPT2 tokenizer from huggingface
     """
 
+    size_tokens = [s.token for s in SIZES]
+
     if tokenizer:
         tokenizer.add_special_tokens({'bos_token': '[P2]',
                                       'additional_special_tokens': ['[P1]',
                                                                     '[P3]',
-                                                                    '[S]',
-                                                                    '[M]',
-                                                                    '[L]',
                                                                     '[T]',
                                                                     '[Sum]',
-                                                                    '[Ent]']})
+                                                                    '[Ent]'] + size_tokens})
     if model:
         model.resize_token_embeddings(len(tokenizer))
