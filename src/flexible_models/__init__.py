87048e894a10797662248d4c0011ee3e9550cf54
Gaël de Léséleuc <dldk.gael@gmail.com>
2020-04-07 16:27:52 +0200

    remove import

--- src/flexible_models/__init__.py
+++ src/flexible_models/__init__.py
@@ -1,7 +1,7 @@
 from .flexible_bert_ner import FlexibleBERTNER
 
-from .flexible_model import FlexibleSummarizer
-from .flexible_bert_sum import FlexibleBERTSum
+#from .flexible_model import FlexibleSummarizer
+#from .flexible_bert_sum import FlexibleBERTSum
 
 from .flexible_bert_embed import FlexibleBERTEmbed
 
