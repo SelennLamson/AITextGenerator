ef4d57de321f8efa6dfa2292f9100dd9be6965b1
Gaël de Léséleuc <dldk.gael@gmail.com>
2020-04-07 16:45:14 +0200

    remove bertsmall

--- src/model_evaluation/gpt2_evaluation.py
+++ src/model_evaluation/gpt2_evaluation.py
@@ -191,6 +191,7 @@ class GPT2EvaluationScript:
 
         if compute_entites_iou:
             bert_ner_model = FlexibleBERTNER(BERT_NER_LARGE, batch_size=self.batch_size)
+
             ent_ious = entities_iou(originals, generated, bert_ner_model)
 
             ent_ious = np.sum([ent_ious[key] for key in ENTITY_TAGS], axis=0) / len(ENTITY_TAGS)
@@ -240,7 +241,7 @@ class GPT2EvaluationScript:
             tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
             model = BertForNextSentencePrediction.from_pretrained('bert-base-uncased').to(device)
 
-            gen_relationships = bert_relationship(generated, P3, model, tokenizer, self.batch_size)
+            gen_relationships = bert_relationship(generated, P3, model, tokenizer, self.batch_size).astype(float)
             #ori_relationships = bert_relationship(originals, P3, model, tokenizer, self.batch_size)
             #normalized_relationships = gen_relationships / ori_relationships #TODO will do shit if ori == 0
 
@@ -250,7 +251,7 @@ class GPT2EvaluationScript:
             if verbose:
                 print("\rRegistering BERT relationship results...", end="")
             for i, relationship in enumerate(gen_relationships):
-                per_paragraph[i]['bert_relationship'] = str(relationship)
+                per_paragraph[i]['bert_relationship'] = relationship
             register_stats(gen_relationships, 'bert_relationship')
 
         print("RESULTS:")
