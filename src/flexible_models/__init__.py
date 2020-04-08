543c3a3c88a291b62b50424f72555881dc5b3608
Gaël de Léséleuc <dldk.gael@gmail.com>
2020-04-08 10:20:40 +0200

    remove import

--- src/flexible_models/__init__.py
+++
@@ -1,9 +0,0 @@
-from .flexible_bert_ner import FlexibleBERTNER
-
-#from .flexible_model import FlexibleSummarizer
-#from .flexible_bert_sum import FlexibleBERTSum
-
-from .flexible_bert_embed import FlexibleBERTEmbed
-
-from .paragraph_parser import ParagraphParser
-from .flexible_GPT2 import FlexibleGPT2
