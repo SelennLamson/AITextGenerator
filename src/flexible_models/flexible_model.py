67c2f6805908ba1a68b13e92f410af49638b25d2
Gaël de Léséleuc <dldk.gael@gmail.com>
2020-04-08 22:20:21 +0200

    remove flexible summarizer

--- src/flexible_models/flexible_model.py
+++ src/flexible_models/flexible_model.py
@@ -10,14 +10,3 @@ class FlexibleModel(ABC):
 
 	def __call__(self, *args, **kwargs):
 		return self.predict(*args, **kwargs)
-
-
-class FlexibleSummarizer(ABC):
-	def __init__(self):
-		pass
-
-	def predict(self, inputs:List[str]) -> List[str]:
-		pass
-
-	def __call__(self, *args, **kwargs):
-		return self.predict(*args, **kwargs)
