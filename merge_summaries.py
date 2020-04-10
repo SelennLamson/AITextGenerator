782b9d1dc018a8e0a644c8600c5414f635654973
Alexandre duval <alexandre.duval@student-cs.fr>
2020-04-10 17:27:09 +0200

    save files with merge summaries

--- merge_summaries.py
+++ merge_summaries.py
@@ -12,21 +12,24 @@ def merge_summaries():
 	# Loop on all books. Look at original json file + corresponding files but with a summary (x4)
 	files = os.listdir(PREPROC_PATH)
 	for f in files:
-		d_id = f[:-len(PREPROC_SUFFIX)]
-		data = json.load(open(PREPROC_PATH + d_id + PREPROC_SUFFIX, 'r'))
-		data_t5 = json.load(open(FOLDER_NAME_T5 + PREFIX_T5 + d_id + PREPROC_SUFFIX, 'r'))
-		data_bart = json.load(open(FOLDER_NAME_BART + PREFIX_BART + d_id + PREPROC_SUFFIX, 'r'))
-		data_pysum = json.load(open(FOLDER_NAME_PYSUM + PREFIX_PYSUM + d_id + PREPROC_SUFFIX, 'r'))
-		data_kw = json.load(open(FOLDER_NAME_KW + PREFIX_KW + d_id + PREPROC_SUFFIX, 'r'))
+		if PREPROC_SUFFIX in f:
+			d_id = f[:-len(PREPROC_SUFFIX)]
+			data = json.load(open(PREPROC_PATH + d_id + PREPROC_SUFFIX, 'r'))
+			data_t5 = json.load(open(FOLDER_NAME_T5 + PREFIX_T5 + d_id + PREPROC_SUFFIX, 'r'))
+			data_bart = json.load(open(FOLDER_NAME_BART + PREFIX_BART + d_id + PREPROC_SUFFIX, 'r'))
+			data_pysum = json.load(open(FOLDER_NAME_PYSUM + PREFIX_PYSUM + d_id + PREPROC_SUFFIX, 'r'))
+			data_kw = json.load(open(FOLDER_NAME_KW + PREFIX_KW + d_id + PREPROC_SUFFIX, 'r'))
 
-		# Add summary from each summariser to the original preproc json file
-		for i in range(len(data['paragraphs'])):
-			data['paragraphs'][i]['summaries'] = dict()
-			data['paragraphs'][i]['summaries'].update(data_t5['paragraphs'][i]['summaries'])
-			data['paragraphs'][i]['summaries'].update(data_bart['paragraphs'][i]['summaries'])
-			data['paragraphs'][i]['summaries'].update(data_pysum['paragraphs'][i]['summaries'])
-			data['paragraphs'][i]['summaries'].update(data_kw['paragraphs'][i]['summaries'])
+			# Add summary from each summariser to the original preproc json file
+			for i in range(len(data['paragraphs'])):
+				data['paragraphs'][i]['summaries'] = dict()
+				data['paragraphs'][i]['summaries'].update(data_t5['paragraphs'][i]['summaries'])
+				data['paragraphs'][i]['summaries'].update(data_bart['paragraphs'][i]['summaries'])
+				data['paragraphs'][i]['summaries'].update(data_pysum['paragraphs'][i]['summaries'])
+				data['paragraphs'][i]['summaries'].update(data_kw['paragraphs'][i]['summaries'])
 
-
+			# Save modifications to preproc json files
+			json.dump(data, open(PREPROC_PATH + d_id + PREPROC_SUFFIX, 'w', encoding='utf-8'), ensure_ascii=False,
+			          indent=1)
 
 merge_summaries()
