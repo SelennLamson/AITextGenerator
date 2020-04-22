04adc38c367b5771f32de6e1d494c4deb9c70ace
Weazel <perso@thomas-lamson.com>
2020-04-22 18:10:30 +0200

    Better entity count metrics, not using BERT-NER anymore

--- src/model_evaluation/metrics/entities_count.py
+++ src/model_evaluation/metrics/entities_count.py
@@ -18,7 +18,7 @@ class EntitiesCount(Metrics):
         :param batch_size: [int] batch size to used for bert
         """
         super().__init__()
-        self.ner_model = FlexibleBERTNER(kwargs['path_to_bert_ner'], batch_size=kwargs['batch_size'])
+        # self.ner_model = FlexibleBERTNER(kwargs['path_to_bert_ner'], batch_size=kwargs['batch_size'])
 
     def __call__(self, predicted_sentences, original_contexts):
         """
@@ -27,23 +27,30 @@ class EntitiesCount(Metrics):
         :return: pd.DataFrame containing
             - proportion of correct entities for each class / each pred_P2
         """
-        entities_in_preds = self.ner_model.predict(predicted_sentences, verbose=0)
+        # entities_in_preds = self.ner_model.predict(predicted_sentences, verbose=0)
 
         df_proportion = pd.DataFrame(columns=["proportion_of_"+class_name for class_name in ENTITY_CLASSES],
                                      data=np.zeros((len(predicted_sentences), len(ENTITY_CLASSES))))
 
-        def filter_by_class(entity_list, class_tag):
-            return set(entity for entity, value in entity_list.items() if value[0] == class_tag)
+        # def filter_by_class(entity_list, eclass_tag):
+        #     return set(entity for entity, value in entity_list.items() if value[0] == eclass_tag)
+        #
+        # for i, (entities_in_pred, original_context) in enumerate(zip(entities_in_preds, original_contexts)):
+        #     for class_tag, class_name in zip(ENTITY_TAGS, ENTITY_CLASSES):
+        #         entities_of_class_tag_in_pred = filter_by_class(entities_in_pred, class_tag)
+        #         entities_in_class_tag_in_true = set(original_context.to_dict()[class_name])
+        #
+        #         proportion = -1 if len(entities_in_class_tag_in_true) == 0 \
+        #                         else len(entities_in_class_tag_in_true.intersection(entities_of_class_tag_in_pred)) / \
+        #                              len(entities_in_class_tag_in_true)
+        #
+        #         df_proportion.loc[i, "proportion_of_"+class_name] = proportion
 
-        for i, (entities_in_pred, original_context) in enumerate(zip(entities_in_preds, original_contexts)):
-            for class_tag, class_name in zip(ENTITY_TAGS, ENTITY_CLASSES):
-                entities_of_class_tag_in_pred = filter_by_class(entities_in_pred, class_tag)
-                entities_in_class_tag_in_true = set(original_context.to_dict()[class_name])
-
-                proportion = -1 if len(entities_in_class_tag_in_true) == 0 \
-                                else len(entities_in_class_tag_in_true.intersection(entities_of_class_tag_in_pred)) / \
-                                     len(entities_in_class_tag_in_true)
-
-                df_proportion.loc[i, "proportion_of_"+class_name] = proportion
+        for i, (predicted_sentence, original_context) in enumerate(zip(predicted_sentences, original_contexts)):
+            cleaned = predicted_sentence.lower().strip()
+            for class_name in ENTITY_CLASSES:
+                ents_in_context = set(original_context.to_dict()[class_name])
+                prop = -1 if len(ents_in_context) == 0 else len([ent for ent in ents_in_context if ent.lower() in cleaned]) / len(ents_in_context)
+                df_proportion.loc[i, "proportion_of_"+class_name] = prop
 
         return df_proportion
