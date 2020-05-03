c3804088e3efb1b52d2980e2f54d5df8a38e757f
Alexandre duval <alexandre.duval@student-cs.fr>
2020-05-03 16:06:44 +0200

    docu fct

--- src/json_generation/prepare_data.py
+++ src/json_generation/prepare_data.py
@@ -5,6 +5,15 @@ from src.utils import *
 
 
 def prepare_data(files: List[str] = None, do_ner=True, do_split=True, verbose=0):
+    """
+    Perform NER on the selected json files, stored in 'data/metadata/files/'. Save them into 'data/novel/'
+    Split into paragraphs the full text of the selected files contained in 'data/novel/'
+    and save them into 'data/preproc/'.
+    :param files: list of the files ids (refer clean_data file) that will be preprocessed
+    :param do_ner: do we perform NER or not
+    :param do_split: do we split the book's full text into paragraphs or not
+    :param verbose: print information
+    """
     if files is None:
         if do_ner:
             files = [f[:-len(METADATA_SUFFIX)] for f in os.listdir(METADATA_PATH)]
